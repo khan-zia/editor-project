00ffcc155f80c5141ffdc17b70f8a3b05553ea8a
Zia Ur Rehman Khan <zmajrohi323@gmail.com>
2022-10-26 21:17:13 +0500

    Configure firestore and bump some deps to latest version.

--- apps/backend/firebase.ts
+++ apps/backend/firebase.ts
@@ -1,11 +1,10 @@
-import admin from 'firebase-admin';
+import admin, { ServiceAccount } from 'firebase-admin';
 
 // Replace this file with your service account key you get when setting up a firestore.
 import serviceAccount from './serviceAccountKey.json';
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://databaseName.firebaseio.com',
+  credential: admin.credential.cert(serviceAccount as ServiceAccount),
 });
 
 export default admin.firestore();
